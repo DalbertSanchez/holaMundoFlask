b7ca09d1a3c23e8a421c817cad419b1a55599ddd
Dalbert Sánchez <sdalbert34@gmail.com>
2025-08-06 15:33:22 -0400

    Fix puerto para despliegue en Render

--- app.py
+++ app.py
@@ -4,7 +4,8 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return "¡Hola Mundo desde Flask y Docker, Dalbert Sanchez 2023-0213!"
+    return "¡Hola Mundo desde Flask en Render y Docker, Dalbert Sanchez 2023-0213!"
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+if __name__ == '__main__':
+    port = int(os.environ.get('PORT', 10000))  # Usa el puerto de la variable de entorno
+    app.run(host='0.0.0.0', port=port)
